07b112a66dbe6da5cd1c2786c5b7c22fc5338071
vishal-hub-git <57080465+vishal-hub-git@users.noreply.github.com>
2020-07-21 21:17:38 +0530

    Update digit_rec.py

--- digit_rec.py
+++ digit_rec.py
@@ -5,7 +5,6 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-print(x_train.shape, y_train.shape)
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 input_shape = (28, 28, 1)
@@ -15,9 +14,6 @@ x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 x_train /= 255
 x_test /= 255
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
 batch_size = 128
 num_classes = 10
 epochs = 10
@@ -33,8 +29,6 @@ model.add(Dense(num_classes, activation='softmax'))
 model.compile(loss=keras.losses.categorical_crossentropy,optimizer=keras.optimizers.Nadam(),metrics=['accuracy'])
 hist = model.fit(x_train, y_train,batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(x_test, y_test))
 print("The model has successfully trained")
-model.save('mnist.h5')
-print("Saving the model as mnist.h5")
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
